5f8359f52a81ea1a29c2ded99a6d9fcc83b4183e
Frotaam <154744342+Frotam@users.noreply.github.com>
2024-01-26 13:37:23 +0530

    Add files via upload

---
+++ js/1_basic/3_operators.js
@@ -0,0 +1,37 @@
+//types of operators 
+// Aretmatic Operators
+// + , - , * , / , % these are the basic operators 
+//  ** =exponential if.e 3**2 the outpt will be 9 
+ 
+// x++ first the x will be displayes and then 1 will be added to it
+// ,x-- first the x will be displayes and then 1 will be subtracted to it
+// ,++x first 1 will be added to it and then new x will be displayed 
+// ,--x first 1 will be sutraced  to it and then new x will be displayed 
+
+
+
+//************************************************************************************************************************* */
+//assignment operator
+// x=y   = assignes the value
+// x-y   -=
+// x+y   +=
+// x*y   *=
+// x/y   /=
+// x%y   %=
+// x**y   **=
+
+
+
+//****************************************comparison operator******************************************* */
+// == equals to 
+// != no equals to 
+// === equal value and type
+// !== not equal value and type
+{/* < > <= >=   */}
+//  ternary operator ?
+//  it is used as if else statment if else i.e 
+//   let b=1
+//  here statmen before ? in () act as congition and statment after ? act as if and statment after : act as else 
+//   let c=(b===2)?5:6
+//   console.log(c)
+
