6c52c9aa11b77736d684bda5e7437fbc15c654d2
Frotaam <154744342+Frotam@users.noreply.github.com>
2024-01-26 13:37:51 +0530

    Delete js programs form basic to improvment directory

--- js programs form basic to improvment/2_basic - Copy/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-console.log(Array.from(Object.values({ name:'sidhant',   // makes the array of values 
-// another_Name: 'hitesh'
-// })))
-
-
-// console.log(Array.from(Object.keys({ name:'sidhant',
-// another_Name: 'hitesh'
-// })))
