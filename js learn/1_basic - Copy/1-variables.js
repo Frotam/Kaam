b00bbe6c1fd7203e5f5da496c6c1fd4ce14acc6a
Frotaam <154744342+Frotam@users.noreply.github.com>
2024-01-26 13:39:58 +0530

    js learn

--- js learn/1_basic - Copy/1-variables.js
+++
@@ -1,9 +0,0 @@
-//There are 3 types of variables i.e  
-let name ="hi"; // now days it is used as it is more efficicent as compare to var and it can  be changed
-const division =1; // const is used to keep the vale of the particular thing  constant i.e it can not be changed
-var section ="c"; // this also works as let i.e stores thee value  
-
-
-// var vs let
-// In the early days of JavaScript, when you used var to declare a variable inside a function, that variable was function-scoped. However, due to hoisting, the variable declaration was moved to the top of its containing function or global scope during the compilation phase. This sometimes led to unexpected behavior, as the variable seemed to be accessible throughout the entire function, even before its actual declaration.
-
