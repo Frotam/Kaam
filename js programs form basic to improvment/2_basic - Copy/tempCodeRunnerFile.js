ff433e000055fa0535dd91d8d445a7a8a5e5c689
Frotaam <154744342+Frotam@users.noreply.github.com>
2024-01-26 13:27:43 +0530

    Add files via upload

---
+++ js programs form basic to improvment/2_basic - Copy/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+console.log(Array.from(Object.values({ name:'sidhant',   // makes the array of values 
+// another_Name: 'hitesh'
+// })))
+
+
+// console.log(Array.from(Object.keys({ name:'sidhant',
+// another_Name: 'hitesh'
+// })))
